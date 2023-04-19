da774efdbeec90098cf7a592a078f2cf1c680dc5
Isram73 <124229203+Isram73@users.noreply.github.com>
2023-04-18 22:49:55 -0600

    Add files via upload

---
+++ ClasesyObjetos.py
@@ -0,0 +1,28 @@
+#Crhistopher Isram Mancilla Laure
+#Clases y objetos
+
+#Ejemplo1
+class Auto:
+    marca = ""
+    modelo = 2004
+    placa = ""
+
+taxi = Auto()
+print(taxi.modelo)
+
+#Ejemplo2
+class persona:
+    doctor = "Victor"
+
+print(persona.doctor)
+
+#Ejemplo3
+class jugadores_A:
+    j1 = "Messi"
+    j2 = "C.Ronaldo"
+
+class jugadores_B:
+    J3 = "Marcelo"
+    j1 = "Falcao"
+
+print(jugadores_B.J3)
